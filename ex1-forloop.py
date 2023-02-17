69e8fe1fc5ce0191c281e873ca0f818e969bbcfd
Utuedor Binah <doctorbinah@yahoo.com>
2023-02-17 20:52:55 +0100

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the value of n: "))
+
+def count_to_n(n = 10):
+    for i in range(i, n+1):
+        print(i, end=' ')
+        print()
+
+print("going to count {} . . .".format(n))
+count_to_n(n)
+#.............................................#
+print("first modification for secoond commit")
---
+++ ex2-dfunction.py
@@ -0,0 +1,7 @@
+def double(n):
+    return 2 * n
+
+print("To call this function using its name")
+print("The programme ran successfully")
+
+print("first modification for secoond commit")
